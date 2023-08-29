6dcb34b31cfcd8ed5441fa71573bd25c7259c9dd
T S Pradeep Kumar <pradeepkumarts@gmail.com>
2023-08-29 13:28:02 +0530

    Add files via upload

---
+++ first.cc
@@ -0,0 +1,102 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/network-module.h"
+#include "ns3/point-to-point-module.h"
+
+#include "ns3/netanim-module.h"
+
+// Default Network Topology
+//
+//       10.1.1.0
+// n0 -------------- n1
+//    point-to-point
+//
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("FirstScriptExample");
+
+int
+main(int argc, char* argv[])
+{
+    CommandLine cmd(__FILE__);
+    cmd.Parse(argc, argv);
+
+    Time::SetResolution(Time::NS);
+    LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+    LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+
+    NodeContainer nodes;
+    nodes.Create(2);
+
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("50Mbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("5ms"));
+
+    NetDeviceContainer devices;
+    devices = pointToPoint.Install(nodes);
+
+    InternetStackHelper stack;
+    stack.Install(nodes);
+
+    Ipv4AddressHelper address;
+    address.SetBase("10.1.1.0", "255.255.255.0");
+
+    Ipv4InterfaceContainer interfaces = address.Assign(devices);
+
+    UdpEchoServerHelper echoServer(8080);
+
+/* nodes.Get(0) is the first node and the second node is
+   nodes.Get(1)
+   */
+    ApplicationContainer serverApps = echoServer.Install(nodes.Get(0));
+    serverApps.Start(Seconds(1.0));
+    serverApps.Stop(Seconds(20.0));
+
+    UdpEchoClientHelper echoClient(interfaces.GetAddress(0), 8080);
+    echoClient.SetAttribute("MaxPackets", UintegerValue(10));
+    echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+    echoClient.SetAttribute("PacketSize", UintegerValue(512));
+
+    ApplicationContainer clientApps = echoClient.Install(nodes.Get(1));
+    clientApps.Start(Seconds(2.0));
+    clientApps.Stop(Seconds(20.0));
+
+//For ascii trace
+AsciiTraceHelper ascii;
+pointToPoint.EnableAsciiAll(ascii.CreateFileStream("512.tr"));
+
+//NetAnim
+AnimationInterface anim("p2p.xml");
+
+    Simulator::Run();
+    Simulator::Destroy();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
